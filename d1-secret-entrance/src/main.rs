ef7c5f918409ee140808f6a60ffacb2b497a971e
Walker Crouse <walker.crouse@acuitybrands.com>
2025-12-08 14:18:11 -0500

    d1 part 1
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

--- d1-secret-entrance/src/main.rs
+++ d1-secret-entrance/src/main.rs
@@ -1,4 +1,6 @@
-const INPUT: &str = include_str!("../input_example.txt");
+const INPUT: &str = include_str!("../input.txt");
+const MAX_POSITION: usize = 99;
+const INITIAL_POSITION: usize = 50;
 
 #[derive(Debug)]
 struct Rotation {
@@ -6,18 +8,37 @@ struct Rotation {
     distance: usize,
 }
 
-fn main() {
-    let rotations = INPUT
-        .lines()
-        .map(|line| {
-            let direction = &line[0..1];
-            let distance = line[1..].parse::<usize>().unwrap();
-            Rotation {
-                right: direction == "R",
-                distance,
-            }
-        })
-        .collect::<Vec<Rotation>>();
+impl From<&str> for Rotation {
+    fn from(line: &str) -> Self {
+        let direction = &line[0..1];
+        let distance = line[1..].parse::<usize>().unwrap();
+        Rotation {
+            right: direction == "R",
+            distance,
+        }
+    }
+}
 
-    println!("{:?}", rotations);
+fn main() {
+    let rotations = INPUT.lines().map(Rotation::from).collect::<Vec<Rotation>>();
+    let mut position: usize = INITIAL_POSITION;
+    let mut password = 0;
+
+    for rotation in &rotations {
+        let new_position = if rotation.right {
+            (position + rotation.distance) % (MAX_POSITION + 1)
+        } else {
+            (position + MAX_POSITION + 1 - rotation.distance % (MAX_POSITION + 1))
+                % (MAX_POSITION + 1)
+        };
+
+        println!("Moving from {} to {}", position, new_position);
+        position = new_position;
+
+        if position == 0 {
+            password += 1;
+        }
+    }
+
+    println!("Password: {}", password);
 }
--- target/debug/deps/d1_secret_entrance-22d9dd2ec670f4cd.d
+++ target/debug/deps/d1_secret_entrance-22d9dd2ec670f4cd.d
@@ -1,9 +1,9 @@
-/Users/wzs02/code/atrius/advent-of-code-25/target/debug/deps/d1_secret_entrance-22d9dd2ec670f4cd.d: d1-secret-entrance/src/main.rs d1-secret-entrance/src/../input_example.txt Cargo.toml
+/Users/wzs02/code/atrius/advent-of-code-25/target/debug/deps/d1_secret_entrance-22d9dd2ec670f4cd.d: d1-secret-entrance/src/main.rs d1-secret-entrance/src/../input.txt Cargo.toml
 
-/Users/wzs02/code/atrius/advent-of-code-25/target/debug/deps/libd1_secret_entrance-22d9dd2ec670f4cd.rmeta: d1-secret-entrance/src/main.rs d1-secret-entrance/src/../input_example.txt Cargo.toml
+/Users/wzs02/code/atrius/advent-of-code-25/target/debug/deps/libd1_secret_entrance-22d9dd2ec670f4cd.rmeta: d1-secret-entrance/src/main.rs d1-secret-entrance/src/../input.txt Cargo.toml
 
 d1-secret-entrance/src/main.rs:
-d1-secret-entrance/src/../input_example.txt:
+d1-secret-entrance/src/../input.txt:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=
