0ec36120877d187cda6957b82f9d9a2e01ce0d30
Walker Crouse <walker.crouse@acuitybrands.com>
2025-12-10 10:50:01 -0500

    day 2 part 1
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

---
+++ d2-p1-gift-shop/src/main.rs
@@ -0,0 +1,64 @@
+const INPUT: &str = include_str!("../input.txt");
+const INVALID_FORMAT: &str = "invalid format";
+
+type Range = (u64, u64);
+
+fn main() {
+    let ranges: Vec<(u64, u64)> = INPUT.split(',').map(parse_range).collect();
+    let mut answer: u64 = 0;
+
+    for range in ranges {
+        for id in range.0..=range.1 {
+            if !is_valid_id(id) {
+                answer += id;
+            }
+        }
+    }
+
+    println!("Answer: {}", answer);
+}
+
+fn parse_range(value: &str) -> Range {
+    let (start, end) = value.split_once('-').expect(INVALID_FORMAT);
+    let start = start.parse::<u64>().expect(INVALID_FORMAT);
+    let end = end.parse::<u64>().expect(INVALID_FORMAT);
+    (start, end)
+}
+
+fn is_valid_id(id: u64) -> bool {
+    let id_str = id.to_string();
+    let num_digits = id_str.len();
+    let digits = id_str.chars();
+    let mut sequence = String::new();
+
+    for (i, digit) in digits.enumerate() {
+        if !sequence.is_empty() {
+            // Check current sequence
+            let remaining_digits = num_digits - i;
+            if remaining_digits == sequence.len() {
+                // We can fit a second repeat of the sequence in the remaining digits
+                if id_str[i..] == sequence {
+                    // The sequence repeats a second time
+                    return false;
+                }
+            }
+        }
+        sequence.push(digit);
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_valid_id_repeated_sequence() {
+        assert!(!is_valid_id(11));
+        assert!(!is_valid_id(1212));
+        assert!(!is_valid_id(123123));
+        assert!(!is_valid_id(1188511885));
+        assert!(is_valid_id(111));
+    }
+}
