f3e2fcff7fd138b71b478d523a98a5b1bff0e553
Walker Crouse <walker.crouse@acuitybrands.com>
2025-12-11 10:49:39 -0500

    add script to create new puzzle crates
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

---
+++ d5-p2-cafeteria/src/main.rs
@@ -0,0 +1,6 @@
+const INPUT: &str = include_str!("../input_example.txt");
+
+fn main() {
+    env_logger::builder().format_timestamp(None).init();
+    println!("Hello, world!");
+}
---
+++ scripts/new_puzzle.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o pipefail
+
+if [ -d "$1" ]; then
+    echo "Error: Directory '$1' already exists"
+    exit 1
+fi
+
+cargo new "$1"
+cargo add --package "$1" log env_logger
+
+cat > ./"$1"/src/main.rs << 'EOF'
+const INPUT: &str = include_str!("../input_example.txt");
+
+fn main() {
+    env_logger::builder().format_timestamp(None).init();
+    println!("Hello, world!");
+}
+EOF
+
+jq --arg name "$1" '.configurations = [{
+  "name": ("Debug executable " + $name),
+  "type": "lldb",
+  "request": "launch",
+  "env": {
+    "RUST_BACKTRACE": "full",
+    "RUST_LOG": "debug"
+  },
+  "cargo": {
+    "args": ["run", ("--bin=" + $name), ("--package=" + $name)]
+  },
+  "args": []
+}] + .configurations' .vscode/launch.json > /tmp/launch.json \
+    && mv /tmp/launch.json .vscode/launch.json
+
+npx prettier --write .vscode/launch.json
